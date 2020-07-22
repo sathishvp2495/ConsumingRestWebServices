a1e662eff2e60acb2dfa92930506885994cbf5a6
sathish <sathishvp2495@gmail.com>
2020-07-22 22:21:08 +0530

    Initial commit

---
+++ src/test/java/com/example/ConsumingRestWebServices/ConsumingAResTfulWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ConsumingRestWebServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumingAResTfulWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
